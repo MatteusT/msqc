b05935fcd503ee3dcae697761c63c219fb8da960
unknown <Matteus@.(none)>
2012-09-03 11:19:50 -0400

    put a max bondlength to the obj.bonded

--- @Model3/bonded.m
+++ @Model3/bonded.m
@@ -6,21 +6,24 @@ if (nargin < 4)
    z2 = obj.Z(jatom);
    if ((z1 == 1) && (z2 == 1))
       maxbond = 0.95;
+      minbond = 0.2;
    elseif (((z1 == 1) && (z2 == 6)) || ((z1 == 6) && (z2 == 1)))
       maxbond = 1.3;
+      minbond = 0.2;
    else % C and C
       maxbond = 1.75;
+      minbond = 0.2;
    end
 end
 
-distAtom = zeros(obj.natom,obj.natom);
+% distAtom = zeros(obj.natom,obj.natom);
 cart = obj.rcart;
-distAtom(iatom,jatom) = norm(cart(:,iatom)-cart(:,jatom));
+distAtom = norm(cart(:,iatom)-cart(:,jatom));
 
 
 if distAtom == 0
     bonding = 0;
-elseif distAtom < maxbond
+elseif (distAtom < maxbond && distAtom > minbond)
     bonding = 1;
 else
     bonding = 0;
