c0340fce6968079a2f5d2160adba9a62dfbb9682
unknown <Matteus@.(none)>
2012-10-17 15:30:16 -0400

    AtomTyping in model3

---
+++ @Model3/atomTyping.m
@@ -0,0 +1,30 @@
+function  atomTyping(obj)
+for iatom = 1:obj.natom
+    if (obj.aType(iatom) == 6)
+        bondedAtoms = find(obj.isBonded(iatom,1:obj.natom));
+        bAtom = obj.Z(bondedAtoms);
+        if isempty(find(bAtom == 6))
+            obj.aType(iatom) = 6;
+        elseif length(find(bAtom == 6)) == 1
+            obj.aType(iatom) = 601;
+        elseif length(find(bAtom == 6)) == 2
+            obj.aType(iatom) = 602;
+        elseif length(find(bAtom == 6)) == 3
+            obj.aType(iatom) = 603;
+        else
+            obj.aType = 604;
+        end
+    elseif (obj.aType(iatom) == 1)
+        bondedAtoms = find(obj.isBonded(iatom,1:obj.natom));
+        bAtom = obj.Z(bondedAtoms);
+    elseif (find(bAtom == 601))
+        obj.aType(iatom) = 101;
+    elseif (find(bAtom == 602))
+        obj.aType(iatom) = 102;
+    elseif (find(bAtom == 603))
+        obj.aType(iatom) = 103;
+    else
+        obj.aType(iatom) = 1;
+    end
+end
+end
