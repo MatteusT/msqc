6c06b081acbbbcc34d2f494d0297a403829d8d69
MatteusT <mtanha@andrew.cmu.edu>
2012-10-31 17:45:10 -0400

    fixed some bugs

--- @Model3/atomTyping.m
+++ @Model3/atomTyping.m
@@ -2,7 +2,7 @@ function  atomTyping(obj)
 for iatom = 1:obj.natom
     if (obj.aType(iatom) == 6)
         bondedAtoms = find(obj.isBonded(iatom,1:obj.natom));
-        bAtom = obj.Z(bondedAtoms);
+        bAtom = obj.aType(bondedAtoms);
         if isempty(find(bAtom == 6))
             obj.aType(iatom) = 6;
         elseif length(find(bAtom == 6)) == 1
@@ -14,17 +14,21 @@ for iatom = 1:obj.natom
         else
             obj.aType = 604;
         end
-    elseif (obj.aType(iatom) == 1)
+    end
+end
+for iatom = 1:obj.natom
+    if (obj.aType(iatom) == 1)
         bondedAtoms = find(obj.isBonded(iatom,1:obj.natom));
-        bAtom = obj.Z(bondedAtoms);
-    elseif (find(bAtom == 601))
-        obj.aType(iatom) = 101;
-    elseif (find(bAtom == 602))
-        obj.aType(iatom) = 102;
-    elseif (find(bAtom == 603))
-        obj.aType(iatom) = 103;
-    else
-        obj.aType(iatom) = 1;
+        bAtom = obj.aType(bondedAtoms);
+        if (find(bAtom == 601))
+            obj.aType(iatom) = 101;
+        elseif (find(bAtom == 602))
+            obj.aType(iatom) = 102;
+        elseif (find(bAtom == 603))
+            obj.aType(iatom) = 103;
+        else
+            obj.aType(iatom) = 1;
+        end
     end
 end
 end
